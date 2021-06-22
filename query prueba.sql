1135421570a265cbb7dac6f5a8524888d8ef1a9f
Daniel Felipe Lozada Ramirez <67969956+dflr10@users.noreply.github.com>
2021-06-22 10:32:03 -0500

    script actualizado

--- query prueba.sql
+++ query prueba.sql
@@ -1,11 +1,13 @@
 
+
 #Usando el botón de eliminar a la inversa Paciente
 UPDATE persona, paciente SET persona.activo=true 
-WHERE persona.id_persona = paciente.id_persona AND paciente.id_paciente= 2;
+WHERE persona.id_persona = paciente.id_persona AND paciente.id_paciente= 1;
 
 #Usando el botón de eliminar a la inversa Usuario
-UPDATE persona, usuario SET persona.activo=true 
-WHERE persona.id_persona = usuario.id_persona AND usuario.id_usuario= 2;
+UPDATE persona, usuario SET persona.activo=0 
+WHERE persona.id_persona = usuario.id_persona AND usuario.id_usuario= 1;
+
 
 
 
@@ -21,6 +23,8 @@ SELECT * FROM Programa_PyDT;
 
 SELECT * FROM Empresa;
 
+SELECT * FROM HistoriaC;
+
 SELECT * FROM Historial_persona;
 
 SELECT * FROM Historial_usuario;
@@ -28,3 +32,7 @@ SELECT * FROM Historial_usuario;
 SELECT * FROM Historial_paciente;
 
 SELECT * FROM Historial_empresa;
+
+SELECT * FROM Historial_historiaC;
+
+SELECT * FROM Departamento;
